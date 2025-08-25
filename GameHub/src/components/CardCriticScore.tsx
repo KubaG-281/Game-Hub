3b554412c3f29b78b33befe609c521f8a3fe9bc3
KubaG <kuba.grzywacz77@gmail.com>
2025-08-25 14:56:30 +0200

    Add critic score

---
+++ GameHub/src/components/CardCriticScore.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  criticScore: number;
+}
+
+const CardCriticScore = ({ criticScore }: Props) => {
+  return (
+    <div
+      className={`border border-2 border-grey ${
+        criticScore > 90 ? "bg-success" : "bg-danger"
+      } px-2 rounded`}
+    >
+      {criticScore}
+    </div>
+  );
+};
+
+export default CardCriticScore;
--- GameHub/src/components/GameCard.tsx
+++ GameHub/src/components/GameCard.tsx
@@ -1,4 +1,5 @@
 import { type Game, type Platform } from "../hooks/useGames";
+import CardCriticScore from "./CardCriticScore";
 import PlatformListIcon from "./PlatformListIcon";
 
 interface Props {
@@ -7,14 +8,15 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <div className="card">
+    <div className="card border border-grey">
       <img src={game.background_image} className="card-img-top" />
       <div className="card-body">
         <h5 className="card-title">{game.name}</h5>
-        <div className="card-text">
+        <div className="d-flex justify-content-between card-text">
           <PlatformListIcon
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
+          <CardCriticScore criticScore={game.metacritic} />
         </div>
       </div>
     </div>
