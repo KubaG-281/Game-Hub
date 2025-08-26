109c118a8b94536a6d09eda090a90903b23a63b9
KubaG <kuba.grzywacz77@gmail.com>
2025-08-27 01:49:31 +0200

    Change skeletons reponsiveness

--- GameHub/src/components/GameGrid.tsx
+++ GameHub/src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({ gameQuery }: Props) => {
 
   return (
     <div className="container-fluid">
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
         {isLoading
           ? skeletons.map((s) => (
               <div key={s} className="col">
